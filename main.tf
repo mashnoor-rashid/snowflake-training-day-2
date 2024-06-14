d578c1d935120c6b0b91e823eab243d6be51dbbf
Mashnoor Rashid <mashnoor.rashid@sunlife.com>
2024-06-14 14:14:58 -0400

    build: add schema for db

--- main.tf
+++ main.tf
@@ -22,3 +22,9 @@ resource "snowflake_database" "mashnoor_demo_db" {
   name    = "MASHNOOR_DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.mashnoor_demo_db.name
+  name     = "MASHNOOR_DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
