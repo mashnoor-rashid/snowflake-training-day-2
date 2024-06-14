1d3625e1c847d6ee29a7785568c9ba286fa42e1e
Mashnoor Rashid <mashnoor.rashid@sunlife.com>
2024-06-14 14:02:27 -0400

    fix: change db name tf resource

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
+resource "snowflake_database" "mashnoor_demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
